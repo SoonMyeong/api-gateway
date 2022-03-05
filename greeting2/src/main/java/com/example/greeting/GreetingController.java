11d596cf3d3b24cc7e6e8c807abb174241b8dc61
SoonMyeong_notebook <soon_myeong@naver.com>
2022-03-05 19:19:05 +0900

    zuul fallback filter 및 TODO LIST 작성

--- greeting2/src/main/java/com/example/greeting/GreetingController.java
+++ greeting2/src/main/java/com/example/greeting/GreetingController.java
@@ -12,8 +12,6 @@ public class GreetingController {
     @GetMapping("/api/greeting")
     public ResponseEntity<String> greeting(HttpServletRequest request) {
 
-
-        /*TODO 아... 생각해보니 Spring Cloud token relay 기능을 활용하지 않으면 마이크로 서비스들 모두 JWTUtil 클래스를 가지고 있어야한다.. 이는 매우 불편할테니 token relay 를 구성하자*/
 //        String header = request.getHeader(HttpHeaders.AUTHORIZATION);
 //        String token = null;
 //        String username = null;
--- spring-cloud-zuul/src/main/java/com/soon/zuul/filter/CustomZuulPreFilter.java
+++ spring-cloud-zuul/src/main/java/com/soon/zuul/filter/CustomZuulPreFilter.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 import javax.servlet.http.HttpServletRequest;
 
 @Component
-public class CustomZuulFilter extends ZuulFilter {
+public class CustomZuulPreFilter extends ZuulFilter {
     @Override
     public String filterType() {
         return "pre";
@@ -32,6 +32,10 @@ public class CustomZuulFilter extends ZuulFilter {
         HttpServletRequest request = requestContext.getRequest();
         String token = request.getHeader(HttpHeaders.AUTHORIZATION);
 
+        //시큐리티 필터를 거쳐 유효성 검사가 완료된 토큰이다. 이를 ZuulRequestHeader 에 넣어주자.
+        //근데 이렇게 될 경우 모든 마이크로서비스가 jwt 를 파싱할 중복의 클래스를 필요로 하게 된다.
+        //팀 컨벤션만 맞춘다면, 여기서 token 정보를 파싱하여 정보를 헤더에 담아 전달해 줄 경우 굳이 jwt 를 파싱하지 않아도 된다.
+        // 아니 이렇게되면 토큰을 릴레이 할 필요도 없다.
         requestContext.addZuulRequestHeader(HttpHeaders.AUTHORIZATION, token);
         return null;
     }
